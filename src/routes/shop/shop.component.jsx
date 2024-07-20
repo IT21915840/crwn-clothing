5a1400738ab5aebf5a7dcc29fc69a0b91eeb3361
Dinithi Silva <it21915840@my.sliit.lk>
2024-07-20 22:02:40 +0530

    Revert "Store products data in firestore and display products in shop page"
    This reverts commit 72a47a6b03ddf6c1a6f2a942ef75f4f5c49f0ce9.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,27 +1,20 @@
-import { useContext, Fragment } from 'react';
+import { useContext } from 'react';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { ProductsContext } from '../../contexts/products.context';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { products } = useContext(ProductsContext);
 
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className='products-container'>
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
+    <div className='products-container'>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
-    </Fragment>
+    </div>
   );
 };
 
